d06a96b363d5cfc1996035c2973263eb4bf44c0b
Philip Li <philip.li@intel.com>
2025-07-01 21:22:45 +0800

    lib/test_result_definition.rb: add TestResultDefinition class
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/test_result_definition.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
+
+require "#{LKP_SRC}/lib/lkp_path"
+
+class TestResultDefinition
+  def initialize(definitions = nil)
+    definitions ||= LKP::Path.src('etc', 'test-result-definition.yml')
+
+    # {"pass"=>{"*"=>"pass"}, "fail"=>{"*"=>"fail failed", "xfstests"=>"crash crashed warn"}, "skip"=>{"*"=>"skip block", "xfstests"=>"inconsistent_fs"}}
+    @definitions = definitions.instance_of?(Hash) ? definitions : YAML.load_file(definitions)
+
+    regexps = @definitions.map do |type, hash|
+      regexps = hash.map { |k, v| Regexp.new("^#{k == '*' ? '[^.]+' : k}\\.(.+\\.)*(#{v.split(' ').join('|')})$", true) }
+      [type, Regexp.union(regexps)]
+    end
+
+    @regexps = Hash[regexps]
+
+    instance_eval do
+      @regexps.each do |type, regexp|
+        define_singleton_method("#{type}?") do |stat|
+          stat =~ regexp
+        end
+      end
+
+      # rli9 FIXME: what if user specified result as definition key?
+      define_singleton_method(:result?) do |stat|
+        @result_regexp ||= Regexp.union(@regexps.values)
+
+        stat =~ @result_regexp
+      end
+    end
+  end
+
+  def type(stat)
+    return unless result?(stat)
+
+    @regexps.find { |_type, regexp| stat =~ regexp }.first
+  end
+
+  def patterns(type, test)
+    @definitions[type].select { |k, _v| ['*', test].include?(k) } # {"*"=>"fail failed", "xfstests"=>"crash crashed warn"}
+                      .values
+                      .flat_map { |v| v.split(' ') }
+  end
+end
---
+++ spec/test_result_definition_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+require "#{LKP_SRC}/lib/test_result_definition"
+
+describe TestResultDefinition do
+  before(:all) do
+    @test_result_definition = described_class.new('pass' => { '*' => 'pass' }, 'fail' => { '*' => 'fail', 't' => 'crash crashed' },
+                                                  'skip' => { '*' => 'skip block' }, 'skipped' => { '*' => 'skipped' })
+  end
+
+  describe '.initialize' do
+    it 'defines singleton methods' do
+      expect(@test_result_definition).to be_respond_to(:pass?)
+      expect(@test_result_definition).to be_respond_to(:skipped?)
+
+      expect(described_class.new({})).not_to be_respond_to(:pass?)
+    end
+  end
+
+  describe '.pass?' do
+    context 'when given passed stat' do
+      it 'returns true' do
+        expect(@test_result_definition).to be_pass('s.x.pass')
+        expect(@test_result_definition).to be_pass('s.x.y.pass')
+        expect(@test_result_definition).to be_pass('t.x.pass')
+        expect(@test_result_definition).to be_pass('t.x.PaSS')
+        expect(@test_result_definition).to be_pass('t.pass')
+      end
+    end
+
+    context 'when given non passed stat' do
+      it 'returns false' do
+        expect(@test_result_definition).not_to be_pass('s.x.passed')
+        expect(@test_result_definition).not_to be_pass('t.x.failed')
+        expect(@test_result_definition).not_to be_pass('....pass')
+      end
+    end
+  end
+
+  describe '.fail?' do
+    context 'when given failed stat' do
+      it 'returns true' do
+        expect(@test_result_definition).to be_fail('s.x.fail')
+        expect(@test_result_definition).to be_fail('t.x.crash')
+      end
+    end
+
+    context 'when given non failed stat' do
+      it 'returns false' do
+        expect(@test_result_definition).not_to be_fail('s.x.failed')
+        expect(@test_result_definition).not_to be_fail('s.x.crash')
+      end
+    end
+  end
+
+  describe '.skip?' do
+    context 'when given skipped stat' do
+      it 'returns true' do
+        expect(@test_result_definition).to be_skip('s.x.block')
+      end
+    end
+
+    context 'when given non skipped stat' do
+      it 'returns false' do
+        expect(@test_result_definition).not_to be_skip('s.block.failed')
+        expect(@test_result_definition).not_to be_skip('s.x.skipped')
+      end
+    end
+  end
+
+  describe '.result?' do
+    context 'when given a valid stat' do
+      it 'returns true' do
+        expect(@test_result_definition).to be_result('s.x.BLOCK')
+        expect(@test_result_definition).to be_result('s.x.y.z.pass')
+        expect(@test_result_definition).to be_result('s.x_y-z.skipped')
+      end
+    end
+
+    context 'when given an invalid stat' do
+      it 'returns false' do
+        expect(@test_result_definition).not_to be_result('s.block.error')
+        expect(@test_result_definition).not_to be_result('....pass')
+      end
+    end
+  end
+end
