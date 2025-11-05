5a284276ab6b97c8a5b8788bc87d243fe024da86
Yi Lai <yi1.lai@intel.com>
2025-11-05 14:41:57 +0800

    lib/tests/lkvs.sh: add /usr/local/lib64/ to LD_LIBRARY_PATH
    Signed-off-by: Yi Lai <yi1.lai@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/lkvs.sh
+++ lib/tests/lkvs.sh
@@ -86,8 +86,9 @@ runtests()
 {
 	# for glxgears on centos, which is located at /usr/lib64/mesa/glxgears
 	export PATH="$PATH:/usr/lib64/mesa"
-	# libipt.so.2 is installed in /usr/local/lib
+	# libipt.so.2 is installed in /usr/local/lib or /usr/local/lib64
 	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
 
 	cd_benchmark $suite/lkvs/BM
 
